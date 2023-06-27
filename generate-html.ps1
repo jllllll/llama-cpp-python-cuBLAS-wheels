bb45fffc2076f45eb22b276eceb2134c651ffeb2
jllllll <3887729+jllllll@users.noreply.github.com>
2023-06-27 00:54:51 -0500

    Make wheel index PEP compliant attempt 2

--- generate-html.ps1
+++ generate-html.ps1
@@ -1,36 +1,52 @@
 Set-Location $PSScriptRoot
 
 $destinationDir = if (Test-Path $(Join-Path $(Resolve-Path '.') 'docs')) {Join-Path '.' 'docs' -resolve} else {(New-Item 'docs' -ItemType 'Directory').fullname}
+$avxVersions = "AVX","AVX2"
 $cudaVersions = "11.6","11.7","11.8","12.0","12.1"
 $packageVersions = "0.1.62","0.1.66"
 $pythonVersions = "3.7","3.8","3.9","3.10","3.11"
 $supportedSystems = 'linux_x86_64','win_amd64'
 $wheelSource = 'https://github.com/jllllll/llama-cpp-python-cuBLAS-wheels/releases/download/wheels'
-$indexName = 'index'
+$packageName = 'llama_cpp_python'
+$packageNameNormalized = 'llama-cpp-python'
 
+$AVXDir = if (Test-Path $(Join-Path $destinationDir 'AVX')) {Join-Path $destinationDir 'AVX'} else {(New-Item $(Join-Path $destinationDir 'AVX') -ItemType 'Directory').fullname}
+$AVX2Dir = if (Test-Path $(Join-Path $destinationDir 'AVX2')) {Join-Path $destinationDir 'AVX2'} else {(New-Item $(Join-Path $destinationDir 'AVX2') -ItemType 'Directory').fullname}
 
 $indexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 $wheelSource = $wheelSource.TrimEnd('/')
-ForEach ($cudaVersion in $cudaVersions)
+Foreach ($avxVersion in $avxVersions)
 {
-	$cu = $cudaVersion.replace('.','')
-	$cuContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
-	ForEach ($packageVersion in $packageVersions)
+	$subIndexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
+	ForEach ($cudaVersion in $cudaVersions)
 	{
-		ForEach ($pythonVersion in $pythonVersions)
+		$cu = $cudaVersion.replace('.','')
+		$cuContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
+		ForEach ($packageVersion in $packageVersions)
 		{
-			$pyVer = $pythonVersion.replace('.','')
-			ForEach ($supportedSystem in $supportedSystems)
+			ForEach ($pythonVersion in $pythonVersions)
 			{
-				$wheel = if ($pyVer -eq '37') {"llama_cpp_python-$packageVersion+cu$cu-cp$pyVer-cp$pyVer`m-$supportedSystem.whl"} else {"llama_cpp_python-$packageVersion+cu$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
-				$cuContent += "<a href=`"$wheelSource/$wheel`">$wheel</a><br/>`n    "
+				$pyVer = $pythonVersion.replace('.','')
+				ForEach ($supportedSystem in $supportedSystems)
+				{
+					$wheel = if ($pyVer -eq '37') {"$packageName-$packageVersion+cu$cu-cp$pyVer-cp$pyVer`m-$supportedSystem.whl"} else {"$packageName-$packageVersion+cu$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
+					if ($avxVersion -eq 'AVX') {$wheel = $wheel.replace($cu,"$cu`AVX")}
+					$cuContent += "<a href=`"$wheelSource/$wheel`">$wheel</a><br/>`n    "
+				}
 			}
+			$cuContent += "`n    "
 		}
-		$cuContent += "`n    "
+		$cuDir = if (Test-Path $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu")) {Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu"} else {(New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu") -ItemType 'Directory').fullname}
+		$packageDir = if (Test-Path $(Join-Path $cuDir $packageNameNormalized)) {Join-Path $cuDir $packageNameNormalized} else {(New-Item $(Join-Path $cuDir $packageNameNormalized) -ItemType 'Directory').fullname}
+		$subIndexContent += "<a href=`"cu$cu/`">CUDA $cudaVersion</a><br/>`n    "
+		New-Item $(Join-Path $packageDir "index.html") -itemType File -value $($cuContent.TrimEnd() + "`n  </body>`n</html>") -force > $null
+		New-Item $(Join-Path $cuDir "index.html") -itemType File -value $("<!DOCTYPE html>`n<html>`n  <body>`n    <a href=`"$packageNameNormalized/`">$packageName</a>`n  </body>`n</html>") -force > $null
+		if ($avxVersion -eq 'AVX2') {New-Item $(Join-Path $destinationDir "cu$cu.html") -itemType File -value $($cuContent.TrimEnd() + "`n  </body>`n</html>") -force > $null}
 	}
-	$indexContent += "<a href=`"cu$cu`">CUDA $cudaVersion</a><br/>`n    "
-	$cuContent.TrimEnd() + "`n  </body>`n</html>" > $(Join-Path $destinationDir "cu$cu.html")
+	$indexContent += "<a href=`"$avxVersion/`">$avxVersion</a><br/>`n    "
+	New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "index.html") -itemType File -value $($subIndexContent.TrimEnd() + "`n  </body>`n</html>") -force > $null
 }
-$indexContent.TrimEnd() + "`n  </body>`n</html>" > $(Join-Path $destinationDir "$indexName.html")
+New-Item $(Join-Path $destinationDir "index.html") -itemType File -value $($indexContent.TrimEnd() + "`n  </body>`n</html>") -force > $null
+#"<!DOCTYPE html>`n<html>`n  <head>`n    <meta http-equiv=`"refresh`" content=`"0; url='./AVX2/cu$cu'`" />`n  </head>`n  <body>`n    <a href=`"AVX2/cu$cu`">CUDA $cudaVersion</a><br/>`n  </body>`n</html>"
 
 pause
---
+++ host-index.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd docs
+python -m http.server 7860
