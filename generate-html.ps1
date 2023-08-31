d1cdf31977b266258ec1e357bb811daccc1fcf7f
jllllll <3887729+jllllll@users.noreply.github.com>
2023-08-31 18:24:16 -0500

    Add ROCm builds to package index

--- generate-html.ps1
+++ generate-html.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 
 $destinationDir = if (Test-Path $(Join-Path $(Resolve-Path '.') 'index')) {Join-Path '.' 'index' -resolve} else {(New-Item 'index' -ItemType 'Directory').fullname}
 $avxVersions = "AVX","AVX2","AVX512","basic"
-$cudaVersions = "11.6","11.7","11.8","12.0","12.1","12.2","cpu"
+$cudaVersions = "11.6","11.7","11.8","12.0","12.1","12.2","rocm5.4.2","rocm5.5","rocm5.5.1","rocm5.6.1","cpu"
 $packageVersions = "0.1.62","0.1.66","0.1.67","0.1.68","0.1.69","0.1.70","0.1.71","0.1.72","0.1.73","0.1.74","0.1.76","0.1.77","0.1.78","0.1.79","0.1.80","0.1.81","0.1.82","0.1.83"
 $pythonVersions = "3.7","3.8","3.9","3.10","3.11"
 $supportedSystems = 'linux_x86_64','win_amd64'
@@ -22,23 +22,29 @@ Foreach ($avxVersion in $avxVersions)
 	$subIndexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 	ForEach ($cudaVersion in $cudaVersions)
 	{
-		$cu = if ($cudaVersion -eq 'cpu') {$cudaVersion} else {'cu' + $cudaVersion.replace('.','')}
+		if ($cudaVersion.StartsWith('rocm') -and $avxVersion -ne 'AVX2') {continue}
+		$cu = if ($cudaVersion -in 'cpu' -or $cudaVersion.StartsWith('rocm')) {$cudaVersion} else {'cu' + $cudaVersion.replace('.','')}
 		if ($cudaVersion -eq 'cpu') {$wheelURL = $wheelSource.TrimEnd('/') + '/cpu'}
+		if ($cudaVersion.StartsWith('rocm')) {$wheelURL = $wheelSource.TrimEnd('/') + '/rocm'}
 		$cuContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 		$cuContentAlt = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 		ForEach ($packageVersion in $packageVersions)
 		{
 			if (($avxVersion -eq 'basic' -or $cudaVersion -eq 'cpu') -and [version]$packageVersion -lt [version]"0.1.70") {continue}
+			if ($cudaVersion.StartsWith('rocm') -and [version]$packageVersion -lt [version]"0.1.80") {continue}
 			ForEach ($pythonVersion in $pythonVersions)
 			{
+				if ($cudaVersion.StartsWith('rocm') -and $pythonVersion -eq "3.7") {continue}
 				$pyVer = $pythonVersion.replace('.','')
 				ForEach ($supportedSystem in $supportedSystems)
 				{
+					if ($cudaVersion.StartsWith('rocm') -and $cudaVersion.Split('rocm')[-1] -ne '5.5.1' -and $supportedSystem -eq 'win_amd64') {continue}
+					if ($cudaVersion.StartsWith('rocm') -and $cudaVersion.Split('rocm')[-1] -eq '5.5.1' -and $supportedSystem -eq 'linux_x86_64') {continue}
 					$wheelTag = if ($cudaVersion -eq 'cpu') {"+cpu$($avxVersion.ToLower())"} else {"+$cu"}
 					$wheel = if ($pyVer -eq '37') {"$packageName-$packageVersion$wheelTag-cp$pyVer-cp$pyVer`m-$supportedSystem.whl"} else {"$packageName-$packageVersion$wheelTag-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
 					$wheelAlt = if ($pyVer -eq '37') {"$packageNameAlt-$packageVersion$wheelTag-cp$pyVer-cp$pyVer`m-$supportedSystem.whl"} else {"$packageNameAlt-$packageVersion$wheelTag-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
 					$cuContent += "<a href=`"$wheelURL/$wheel`">$wheel</a><br/>`n    "
-					if ($packageVersion -in $packageAltVersions) {$cuContentAlt += "<a href=`"$wheelURL/$wheelAlt`">$wheelAlt</a><br/>`n    "}
+					if ($packageVersion -in $packageAltVersions -and !$cudaVersion.StartsWith('rocm')) {$cuContentAlt += "<a href=`"$wheelURL/$wheelAlt`">$wheelAlt</a><br/>`n    "}
 				}
 			}
 			$cuContent += "`n    "
@@ -46,12 +52,14 @@ Foreach ($avxVersion in $avxVersions)
 		}
 		$cuDir = if (Test-Path $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "$cu")) {Join-Path $(Get-Variable "$avxVersion`Dir").Value "$cu"} else {(New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "$cu") -ItemType 'Directory').fullname}
 		$packageDir = if (Test-Path $(Join-Path $cuDir $packageNameNormalized)) {Join-Path $cuDir $packageNameNormalized} else {(New-Item $(Join-Path $cuDir $packageNameNormalized) -ItemType 'Directory').fullname}
-		$packageAltDir = if (Test-Path $(Join-Path $cuDir $packageNameAltNormalized)) {Join-Path $cuDir $packageNameAltNormalized} else {(New-Item $(Join-Path $cuDir $packageNameAltNormalized) -ItemType 'Directory').fullname}
-		$cuLabel = if ($cudaVersion -eq 'cpu') {$cudaVersion} else {"CUDA $cudaVersion"}
+		$packageAltDir = if (Test-Path $(Join-Path $cuDir $packageNameAltNormalized)) {Join-Path $cuDir $packageNameAltNormalized} elseif (!$cudaVersion.StartsWith('rocm')) {(New-Item $(Join-Path $cuDir $packageNameAltNormalized) -ItemType 'Directory').fullname}
+		$cuLabel = if ($cudaVersion -eq 'cpu') {$cudaVersion} elseif ($cudaVersion.StartsWith('rocm')) {'ROCm'+' '+$cudaVersion.Split('rocm')[-1]} else {"CUDA $cudaVersion"}
 		$subIndexContent += "<a href=`"$cu/`">$cuLabel</a><br/>`n    "
 		New-Item $(Join-Path $packageDir "index.html") -itemType File -value $($cuContent.TrimEnd() + "`n  </body>`n</html>`n") -force > $null
-		New-Item $(Join-Path $packageAltDir "index.html") -itemType File -value $($cuContentAlt.TrimEnd() + "`n  </body>`n</html>`n") -force > $null
-		New-Item $(Join-Path $cuDir "index.html") -itemType File -value $("<!DOCTYPE html>`n<html>`n  <body>`n    <a href=`"$packageNameNormalized/`">$packageName</a><br/>`n    <a href=`"$packageNameAltNormalized/`">$packageNameAlt</a>`n  </body>`n</html>`n") -force > $null
+		if (!$cudaVersion.StartsWith('rocm')) {New-Item $(Join-Path $packageAltDir "index.html") -itemType File -value $($cuContentAlt.TrimEnd() + "`n  </body>`n</html>`n") -force > $null}
+		$packageIndexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    <a href=`"$packageNameNormalized/`">$packageName</a><br/>`n"
+		if (!$cudaVersion.StartsWith('rocm')) {$packageIndexContent += "    <a href=`"$packageNameAltNormalized/`">$packageNameAlt</a>`n"}
+		New-Item $(Join-Path $cuDir "index.html") -itemType File -value $($packageIndexContent + "  </body>`n</html>`n") -force > $null
 		if ($avxVersion -eq 'AVX2') {New-Item $(Join-Path $destinationDir "$cu.html") -itemType File -value $($cuContent.TrimEnd() + "`n  </body>`n</html>`n") -force > $null}
 	}
 	$indexContent += "<a href=`"$avxVersion/`">$avxVersion</a><br/>`n    "
--- generate-textgen-html.ps1
+++ generate-textgen-html.ps1
@@ -3,7 +3,7 @@ Set-Location $PSScriptRoot
 $destinationDir = if (Test-Path $(Join-Path $(Resolve-Path '.') 'index')) {Join-Path '.' 'index' -resolve} else {(New-Item 'index' -ItemType 'Directory').fullname}
 $destinationDir = if (Test-Path $(Join-Path $destinationDir 'textgen')) {Join-Path $destinationDir 'textgen'} else {(New-Item $(Join-Path $destinationDir 'textgen') -ItemType 'Directory').fullname}
 $avxVersions = "AVX","AVX2","basic"
-$cudaVersions = "11.7","11.8","12.0","12.1","12.2"
+$cudaVersions = "11.7","11.8","12.0","12.1","12.2","rocm5.4.2","rocm5.5","rocm5.5.1","rocm5.6.1"
 $packageVersions = "0.1.73","0.1.74","0.1.76","0.1.77","0.1.78","0.1.79","0.1.80","0.1.81","0.1.82","0.1.83"
 $pythonVersions = "3.8","3.9","3.10","3.11"
 $supportedSystems = 'linux_x86_64','win_amd64'
@@ -23,37 +23,45 @@ Foreach ($avxVersion in $avxVersions)
 	$subIndexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 	ForEach ($cudaVersion in $cudaVersions)
 	{
-		$cu = $cudaVersion.replace('.','')
+		if ($cudaVersion.StartsWith('rocm') -and $avxVersion -ne 'AVX2') {continue}
+		if ($cudaVersion.StartsWith('rocm')) {$wheelURL = $wheelSource.TrimEnd('/') + '/rocm'}
+		$cu = if ($cudaVersion.StartsWith('rocm')) {$cudaVersion} else {'cu' + $cudaVersion.replace('.','')}
 		$cuContent = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 		$cuContentAlt = "<!DOCTYPE html>`n<html>`n  <body>`n    "
 		ForEach ($packageVersion in $packageVersions)
 		{
 			if ($avxVersion -eq 'basic' -and $packageVersion -eq '0.1.73') {continue}
+			if ($cudaVersion.StartsWith('rocm') -and [version]$packageVersion -lt [version]"0.1.80") {continue}
 			ForEach ($pythonVersion in $pythonVersions)
 			{
 				$pyVer = $pythonVersion.replace('.','')
 				ForEach ($supportedSystem in $supportedSystems)
 				{
-					$wheel = if ($avxVersion -eq 'AVX') { "$packageName-$packageVersion+cu$cu$('avx')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
-					} elseif ($avxVersion -eq 'basic') { "$packageName-$packageVersion+cu$cu$('basic')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
-					} else {"$packageName-$packageVersion+cu$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
-					$wheelAlt = if ($avxVersion -eq 'AVX') { "$packageNameAlt-$packageVersion+cu$cu$('avx')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
-					} elseif ($avxVersion -eq 'basic') { "$packageNameAlt-$packageVersion+cu$cu$('basic')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
-					} else {"$packageNameAlt-$packageVersion+cu$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
+					if ($cudaVersion.StartsWith('rocm') -and $cudaVersion.Split('rocm')[-1] -ne '5.5.1' -and $supportedSystem -eq 'win_amd64') {continue}
+					if ($cudaVersion.StartsWith('rocm') -and $cudaVersion.Split('rocm')[-1] -eq '5.5.1' -and $supportedSystem -eq 'linux_x86_64') {continue}
+					$wheel = if ($avxVersion -eq 'AVX') { "$packageName-$packageVersion+$cu$('avx')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
+					} elseif ($avxVersion -eq 'basic') { "$packageName-$packageVersion+$cu$('basic')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
+					} else {"$packageName-$packageVersion+$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
+					$wheelAlt = if ($avxVersion -eq 'AVX') { "$packageNameAlt-$packageVersion+$cu$('avx')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
+					} elseif ($avxVersion -eq 'basic') { "$packageNameAlt-$packageVersion+$cu$('basic')-cp$pyVer-cp$pyVer-$supportedSystem.whl"
+					} else {"$packageNameAlt-$packageVersion+$cu-cp$pyVer-cp$pyVer-$supportedSystem.whl"}
 					if (!($packageVersion -eq '0.1.73' -and $avxVersion -eq 'AVX')) {$cuContent += "<a href=`"$wheelURL/$wheel`">$wheel</a><br/>`n    "}
-					if ($packageVersion -in $packageAltVersions) {$cuContentAlt += "<a href=`"$wheelURL/$wheelAlt`">$wheelAlt</a><br/>`n    "}
+					if ($packageVersion -in $packageAltVersions -and !$cudaVersion.StartsWith('rocm')) {$cuContentAlt += "<a href=`"$wheelURL/$wheelAlt`">$wheelAlt</a><br/>`n    "}
 				}
 			}
 			if (!($packageVersion -eq '0.1.73' -and $avxVersion -eq 'AVX')) {$cuContent += "`n    "}
 			if ($packageVersion -in $packageAltVersions) {$cuContentAlt += "`n    "}
 		}
-		$cuDir = if (Test-Path $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu")) {Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu"} else {(New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "cu$cu") -ItemType 'Directory').fullname}
+		$cuDir = if (Test-Path $(Join-Path $(Get-Variable "$avxVersion`Dir").Value $cu)) {Join-Path $(Get-Variable "$avxVersion`Dir").Value $cu} else {(New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value $cu) -ItemType 'Directory').fullname}
 		$packageDir = if (Test-Path $(Join-Path $cuDir $packageNameNormalized)) {Join-Path $cuDir $packageNameNormalized} else {(New-Item $(Join-Path $cuDir $packageNameNormalized) -ItemType 'Directory').fullname}
-		$packageAltDir = if (Test-Path $(Join-Path $cuDir $packageNameAltNormalized)) {Join-Path $cuDir $packageNameAltNormalized} else {(New-Item $(Join-Path $cuDir $packageNameAltNormalized) -ItemType 'Directory').fullname}
-		$subIndexContent += "<a href=`"cu$cu/`">CUDA $cudaVersion</a><br/>`n    "
+		$packageAltDir = if (Test-Path $(Join-Path $cuDir $packageNameAltNormalized)) {Join-Path $cuDir $packageNameAltNormalized} elseif (!$cudaVersion.StartsWith('rocm')) {(New-Item $(Join-Path $cuDir $packageNameAltNormalized) -ItemType 'Directory').fullname}
+		$cuLabel = if ($cudaVersion.StartsWith('rocm')) {'ROCm'+' '+$cudaVersion.Split('rocm')[-1]} else {"CUDA $cudaVersion"}
+		$subIndexContent += "<a href=`"$cu/`">$cuLabel</a><br/>`n    "
 		New-Item $(Join-Path $packageDir "index.html") -itemType File -value $($cuContent.TrimEnd() + "`n  </body>`n</html>`n") -force > $null
-		New-Item $(Join-Path $packageAltDir "index.html") -itemType File -value $($cuContentAlt.TrimEnd() + "`n  </body>`n</html>`n") -force > $null
-		New-Item $(Join-Path $cuDir "index.html") -itemType File -value $("<!DOCTYPE html>`n<html>`n  <body>`n    <a href=`"$packageNameNormalized/`">$packageName</a><br/>`n    <a href=`"$packageNameAltNormalized/`">$packageNameAlt</a>`n  </body>`n</html>`n") -force > $null
+		if (!$cudaVersion.StartsWith('rocm')) {New-Item $(Join-Path $packageAltDir "index.html") -itemType File -value $($cuContentAlt.TrimEnd() + "`n  </body>`n</html>`n") -force > $null}
+		$packageIndexContent = "<!DOCTYPE html>`n<html>`n  <body>`n    <a href=`"$packageNameNormalized/`">$packageName</a><br/>`n"
+		if (!$cudaVersion.StartsWith('rocm')) {$packageIndexContent += "    <a href=`"$packageNameAltNormalized/`">$packageNameAlt</a>`n"}
+		New-Item $(Join-Path $cuDir "index.html") -itemType File -value $($packageIndexContent + "  </body>`n</html>`n") -force > $null
 	}
 	$indexContent += "<a href=`"$avxVersion/`">$avxVersion</a><br/>`n    "
 	New-Item $(Join-Path $(Get-Variable "$avxVersion`Dir").Value "index.html") -itemType File -value $($subIndexContent.TrimEnd() + "`n  </body>`n</html>`n") -force > $null
