70d290b8082b8f8234f1740670c99731d53209f2
jllllll <3887729+jllllll@users.noreply.github.com>
2023-09-09 17:01:52 -0500

    Add 0.1.84 builds to package index

--- generate-html.ps1
+++ generate-html.ps1
@@ -3,7 +3,7 @@ Set-Location $PSScriptRoot
 $destinationDir = if (Test-Path $(Join-Path $(Resolve-Path '.') 'index')) {Join-Path '.' 'index' -resolve} else {(New-Item 'index' -ItemType 'Directory').fullname}
 $avxVersions = "AVX","AVX2","AVX512","basic"
 $cudaVersions = "11.6","11.7","11.8","12.0","12.1","12.2","rocm5.4.2","rocm5.5","rocm5.5.1","rocm5.6.1","cpu"
-$packageVersions = "0.1.62","0.1.66","0.1.67","0.1.68","0.1.69","0.1.70","0.1.71","0.1.72","0.1.73","0.1.74","0.1.76","0.1.77","0.1.78","0.1.79","0.1.80","0.1.81","0.1.82","0.1.83"
+$packageVersions = "0.1.62","0.1.66","0.1.67","0.1.68","0.1.69","0.1.70","0.1.71","0.1.72","0.1.73","0.1.74","0.1.76","0.1.77","0.1.78","0.1.79","0.1.80","0.1.81","0.1.82","0.1.83","0.1.84"
 $pythonVersions = "3.7","3.8","3.9","3.10","3.11"
 $supportedSystems = 'linux_x86_64','win_amd64'
 $wheelSource = 'https://github.com/jllllll/llama-cpp-python-cuBLAS-wheels/releases/download'
